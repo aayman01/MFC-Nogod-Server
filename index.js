60bf18a6a18fd215fc30bd09605984b560ce253a
aayman01 <ayman.alfi098@gmail.com>
2025-02-26 13:32:57 +0600

    login and jwt middleware created

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ async function run() {
     const transactionsCollection = db.collection("transactions");
     const adminCollection = db.collection("admin");
 
+    function authenticateToken(req, res, next) {
+      const token = req.header("Authorization")?.split(" ")[1];
+      if (!token) return res.status(401).json({ message: "Access denied" });
+      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: "Invalid token" });
+        req.user = user;
+        next();
+      });
+    }
+
     // Register User or Agent
     app.post("/register", async (req, res) => {
       const { name, mobile, email, pin, accountType, nid } = req.body;
@@ -55,6 +65,60 @@ async function run() {
       await usersCollection.insertOne(newUser);
       res.status(201).json({ message: "success" });
     });
+
+
+    app.post("/login", async (req, res) => {
+      const { identifier, pin } = req.body; // Single identifier field
+      // console.log(identifier,pin)
+
+      let user;
+      let isEmail = false; 
+
+      // Regular expressions to check if it's an email or a mobile number
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+      const mobileRegex = /^[0-9]{11}$/; 
+
+      if (emailRegex.test(identifier)) {
+        isEmail = true;
+        user = await usersCollection.findOne({ email: identifier });
+      } else if (mobileRegex.test(identifier)) {
+        user = await usersCollection.findOne({ mobile: identifier });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Invalid email or phone number format" });
+      }
+
+      if (!user) {
+        return res.status(400).json({ message: "User not found" });
+      }
+
+      const validPin = await bcrypt.compare(pin, user.pin);
+      if (!validPin) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign(
+        { id: user._id, role: user.accountType },
+        process.env.JWT_SECRET
+      );
+      res.json({ token, user ,message: "success" });
+    });
+
+
+    app.get("/user", authenticateToken, (req, res) => {
+      res.json(req.user); 
+    });
+
+    app.get("/user/:id",async(req, res) => {
+      // const id = req.params.id;
+      console.log(id)
+      const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+      // console.log(user)
+      res.json(user);
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
